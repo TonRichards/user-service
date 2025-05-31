f369a8753b41c1a44c36ee306b6c35e1673366c7
TonRichards <58299854+TonRichards@users.noreply.github.com>
2025-05-31 14:46:33 +0700

    Logout feature

--- routes/apis/auth.php
+++ routes/apis/auth.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Api\Clients\AuthController;
 Route::controller(AuthController::class)->prefix('auth')->group(function () {
     Route::post('/register', 'register');
     Route::post('/login', 'login');
+    Route::post('/logout', 'logout')->middleware(['auth:api']);;
 
     Route::post('/switch-organization', 'switchOrganization')->middleware(['auth:api']);
     Route::get('/check', 'getCurrentUser')->middleware(['auth:api']);
